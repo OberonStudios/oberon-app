8a3fe31df7e511f57527fa246a0b9651f097619c
aakatev <aakatev@mail.sfsu.edu>
2019-09-26 06:02:31 -0700

    CSS changes

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 const HomeComponent = {
   template:`<main role="main" class="inner cover">
-              <h1 class="text-center cover-heading">How awesome is Artem today?</h1>
-              <p class="text-center lead">Today artem is <span id="score">{{ random  }}</span>% awesome!</p>
+              <h1 class="text-center cover-heading">BEAROMETER</h1>
+              <p class="text-center lead">Your score is <span id="score">{{ random  }}</span>%!</p>
               <p class="lead">
                 <div class="progress">
                   <div v-bind:style="{ width: random + '%' }" class="progress-bar bg-success progress-bar-striped progress-bar-animated" id="progressbar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria- valuemax="100"></div>
                 </div>
-                <h2 class="mt-5 text-center" v-if="random>75">🔥🔥🔥</h2>
-                <h2 class="mt-5 text-center" v-else-if="random>40">Not bad 😏</h2>
-                <h2 class="mt-5 text-center" v-else>Oh no 😢</h2>
+                <h2 class="mt-5 text-center" v-if="random>75">Congrats! You are definitely a bear! 🐻</h2>
+                <h2 class="mt-5 text-center" v-else-if="random>40">You might be a panda, boi! 🐼</h2>
+                <h2 class="mt-5 text-center" v-else>Sorry, but you a just a dog! 🐶</h2>
               </p>
             </main>`,
   data: function() {
@@ -18,22 +18,23 @@ const HomeComponent = {
   }
 }
 const FeaturesComponent = {
-  template: `<p class="text-center"><a href="/mockup"> <h2>{{message}}</h2> </a><br>Username: admin<br>Password: 12345</p>`,
-  data: function() { return({ message: `¶Click Me...`})  } }
+  template: `<p class="text-center">{{message}}</p>`,
+  data: function() { return({ message: `¶Insert Paragraph`})  } }
+
 const ContactComponent = {
   template: `<div class="container text-center">
       <div class="row">
         <div class="col-sm">
           <i class="material-icons">local_phone</i>
-          <p>+1(415)-470-0506</p>
+          <p>+X(XXX)-XXX-XXXX</p>
         </div>
         <div class="col-sm">
           <i class="material-icons">email</i>
-          <p>artem.akatev@edduus.com</p>
+          <p>oberonstudios@gmail.com</p>
         </div>
         <div class="col-sm">
           <i class="material-icons">code</i>
-          <p>GitHub: aakatev</p>
+          <p>GitHub: OberonStudios</p>
         </div>
       </div>
     </div>`
